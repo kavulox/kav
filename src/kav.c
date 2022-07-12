b1cee3a7c52a028fba9aaa5cb4c46d4e68f18df1
kavulox <kavulox@gentoo>
2022-07-11 23:06:15 -0400

    Added update

--- src/kav.c
+++ src/kav.c
@@ -6,6 +6,7 @@
 
 
 typedef struct {
+    char *ip_addr;
     int mon_count;
     int screen_w;
     int screen_h;
@@ -13,6 +14,10 @@ typedef struct {
     int term_y;
 } System;
 
+
+void net_grab_ip(System *ptr) {
+    ptr->ip_addr = r_vstr("curl ifconfig.me 2> /dev/null");
+}
 
 void set_res_val(System *ptr, char *str) {
     char *value, *key = strtok_r(str, "x", &value);
@@ -47,10 +52,37 @@ void get_screen_res(System *ptr) {
 
 void toggle_gui(); // will make front end gui   ;
 
-
-int main() {
+void handle_tests(char *argx) {
     System sys, *ptr = &sys;
-    get_screen_res(ptr);
-    set_monitor_count(ptr);
-    printf("%d :: %d :: Monitor Count :: %d\n",ptr->screen_w, ptr->screen_h, ptr->mon_count);
+    if (strcmp(argx, "ip")==0) {
+        net_grab_ip(ptr);
+        printf("Ip :: %s\n",ptr->ip_addr);
+    }
+    else if (strcmp(argx, "monitor-count")==0) {
+        set_monitor_count(ptr);
+        printf("Monitor Count ::  %d\n",ptr->mon_count);
+    }
+    else if (strcmp(argx, "resolution")==0) {
+        get_screen_res(ptr);
+        printf("Screen Resolution :: %dx%d\n",ptr->screen_h, ptr->screen_w);
+    }
+    else if (strcmp(argx, "term-info")==0) {
+        x_term_set(ptr);
+        printf("%d :: %d\n",ptr->term_x, ptr->term_y);
+    }
+    else {
+        printf("Failed To Parse.\n");
+        return;
+    }
 }
+
+
+
+int main(int argc, char **argv) {
+    handle_tests(argv[1]);
+}
+
+
+
+
+
